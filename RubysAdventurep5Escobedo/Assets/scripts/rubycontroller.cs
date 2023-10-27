d23595c771ec08793fffd78b518fd0f5d95e5bda
bruce <753825@student.dusd.net>
2023-10-27 13:48:24 -0700

    added movement to ruby

--- RubysAdventurep5Escobedo/Assets/scripts/rubycontroller.cs
+++ RubysAdventurep5Escobedo/Assets/scripts/rubycontroller.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class rubycontroller : MonoBehaviour
 {
@@ -14,19 +13,11 @@ public class rubycontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position
-        position.x = position.x + 0.1f;
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
         transform.position = position;
     }
-}
-//this line is there to tell me the x position of my object
-
-/*hi there!
- * this is two lines
- * */
-Debug.Log(transform.position.x);
-
-if (transform.position.x <= 5f)
-{
-    Debug.Log("I'm about to hit the ground!");
 }
