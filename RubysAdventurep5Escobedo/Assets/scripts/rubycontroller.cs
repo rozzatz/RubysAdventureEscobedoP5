71cdd94a004495454695fbdc7ce4988998a1f536
bruce <753825@student.dusd.net>
2023-11-08 13:49:10 -0800

    stuff

--- RubysAdventurep5Escobedo/Assets/scripts/rubycontroller.cs
+++ RubysAdventurep5Escobedo/Assets/scripts/rubycontroller.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class rubycontroller : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
-    //START WORLD INTERACTIONS AT 13:07
+
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
+    }
+    void FixedUpdate()
+    {
 
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        //currentHealth = Mathf.Clamp(currentHealth +amount,)
+        //2:44 world interactions collectables
     }
 }
