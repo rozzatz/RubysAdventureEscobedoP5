1e07e0f59d18590f09b1518b04275ad87967b4ad
bruce <753825@student.dusd.net>
2023-11-13 22:27:17 -0800

    updated ruby controller
    added health and speed variables to ruby controller

--- RubysAdventurep5Escobedo/Assets/scripts/rubycontroller.cs
+++ RubysAdventurep5Escobedo/Assets/scripts/rubycontroller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class rubycontroller : MonoBehaviour
 {
+    public float speed = 3.0f;
     public int maxHealth = 5;
     int currentHealth;
     Rigidbody2D rigidbody2d;
@@ -28,14 +29,15 @@ public class rubycontroller : MonoBehaviour
     {
 
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
     void ChangeHealth(int amount)
     {
-        //currentHealth = Mathf.Clamp(currentHealth +amount,)
-        //2:44 world interactions collectables
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+        //9:18 collectables
     }
 }
